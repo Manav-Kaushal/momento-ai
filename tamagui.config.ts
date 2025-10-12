69c2ae78ca1864f1516902d1d3ec91345e95fee7
manavkaushal26 <manav.kaushal@spacejoy.com>
2025-10-12 16:39:16 +0530

    in progress: auth screen

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,12 +1,60 @@
-import { defaultConfig } from '@tamagui/config/v4'
-import { createTamagui } from 'tamagui'
+import { defaultConfig } from "@tamagui/config/v4";
+import { createTamagui, createTokens } from "tamagui";
 
-export const config = createTamagui(defaultConfig)
+// Custom Styles
+const customTokens = createTokens({
+  color: {
+    // Red scale centered on brand primary #ef5d5a
+    red1: "#fff5f5",
+    red2: "#ffebeb",
+    red3: "#ffdede",
+    red4: "#ffcaca",
+    red5: "#ffb2b1",
+    red6: "#ff9b9a",
+    red7: "#ff8382",
+    red8: "#ff6e6b",
+    red9: "#ef5d5a", // primary
+    red10: "#d85451",
+    red11: "#b64642",
+    red12: "#3f1c1c",
+  },
+  radius: defaultConfig.tokens.radius,
+  zIndex: defaultConfig.tokens.zIndex,
+  space: defaultConfig.tokens.space,
+  size: defaultConfig.tokens.size,
+});
 
-export default config
+const baseTheme = defaultConfig.themes.light_blue;
 
-export type Conf = typeof config
+const config = {
+  ...defaultConfig,
+  tokens: customTokens,
+  themes: {
+    ...defaultConfig.themes,
+    red: {
+      ...baseTheme,
+      background: "$red1",
+      backgroundHover: "$red2",
+      backgroundPress: "$red3",
+      backgroundFocus: "$red2",
+      color: "$red12",
+      colorHover: "$red12",
+      colorPress: "$red12",
+      colorFocus: "$red12",
+      borderColor: "$red6",
+      borderColorHover: "$red7",
+      borderColorPress: "$red8",
+      borderColorFocus: "$red7",
+    },
+  },
+};
 
-declare module 'tamagui' {
+export const tamaguiConfig = createTamagui(config);
+
+export default tamaguiConfig;
+
+export type Conf = typeof tamaguiConfig;
+
+declare module "tamagui" {
   interface TamaguiCustomConfig extends Conf {}
 }
